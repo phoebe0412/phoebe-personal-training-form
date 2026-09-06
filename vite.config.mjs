86cd10f9a61a86ac8394301cfed28862424f50c2
Phoebe Chou <phoebechou@PhoebedeMacBook-Pro.local>
2026-09-06 13:29:06 +0800

    Deploy site with GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/phoebe-personal-training-form/' : '/',
   build: {
     outDir: "dist/client",
   },
