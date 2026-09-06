42d5c8259b017437cc3e5710854c4addb780a57c
Phoebe Chou <phoebechou@PhoebedeMacBook-Pro.local>
2026-09-06 21:20:40 +0800

    Support independent GitHub Pages URLs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/phoebe-personal-training-form/' : '/',
+  base: process.env.GITHUB_ACTIONS ? `/${process.env.GITHUB_REPOSITORY?.split('/').at(-1) || 'phoebe-personal-training-form'}/` : '/',
   build: {
     outDir: "dist/client",
   },
