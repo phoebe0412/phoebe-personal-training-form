eb50a80b58358de855cf9db7e1833881603dd4b5
Phoebe Chou <phoebechou@PhoebedeMacBook-Pro.local>
2026-09-06 15:38:58 +0800

    Connect form to Google Sheets endpoint

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// After deploying google-apps-script/Code.gs as a Web App, paste its /exec URL here.
-export const GOOGLE_APPS_SCRIPT_URL = '';
+export const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxSyN9DyzwEGm5cnvyCF_ALRr0OQk4dqe_2vdY8C1slpO6i5Iu52WRMTluOpRURGt0/exec';
